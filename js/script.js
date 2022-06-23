ae8ac4d35aac87ec0a1829c2f63e7d16803da1a4
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-06-23 10:14:58 +0000

    Added getNumberInfo function at script.js

--- js/script.js
+++ js/script.js
@@ -19,3 +19,32 @@ const PRIMES = Array(1000).fill(false);
         if (isPrime[i]) PRIMES[2*i+3] = true;
     }
 })(1000);
+
+// Function to check if the number num is a prime
+function isPrime(num) {
+    if (num<2) return false;
+    if (PRIMES[num] !== undefined) return PRIMES[num];
+    const sqrt = parseInt(Math.sqrt(num));
+    for (let i=2;i<=sqrt;i++) {
+        if (num%i===0) return PRIMES[num] = false;
+    }
+    return PRIMES[num] = true;
+}
+
+// Function to Get Number Information (including Invalid Input)
+function getNumberInfo() {
+    // Set Up Variable
+    let txt = "";
+    // Get the value of the input field
+    let num = document.getElementById("mynumber").value;
+    // Check if input is valid
+    if (isNaN(num) || num.toString().length == 0 || num<2 || num>1000 || !Number.isInteger(Number(num))) {
+        txt += `Invalid input.  Please enter a whole number between 2 and 1000.`;
+    } else {
+        txt += `You have entered the number ${num} as denominator.<p>`;
+        txt += `Value is ${reciprocalCycles(num)} for which 1/${reciprocalCycles(num)} contains the longest recurring cycle in its decimal fraction part.`
+    }
+
+    // Display Information in the Browser
+    document.getElementById("numinfo").innerHTML = txt;
+}
